482ccb3170c9e48004ce1dc93ebe99905769bec9
Brandon Artigue <bartigue87@yahoo.com>
2022-08-15 17:05:01 -0600

    all articles styling changes

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -18,7 +18,7 @@ export default function ArticleItem(props) {
       </div>
       <div className="article-text-container">
         <h1 className="site">{props.title}</h1>
-        <h6>{props.genre}</h6>
+        <h6 className="article-genre">{props.genre}</h6>
         <p className="article-preview">{props.preview}</p>
         <button className="button article-btn" onClick={handleClick}>
           Check it out
